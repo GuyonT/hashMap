7e2c1c677890a940b331d2bd8f9f1c267511c35b
GuyonT <thomas.guyonnet@outlook.fr>
2025-02-28 10:45:18 +0100

    add functions remove, length, clear, keys, values and entries

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.size = 0;
   }
 
   prepend(key, value) {
@@ -17,15 +18,16 @@ class LinkedList {
     if (this.head === null) {
       this.head = newNode;
       this.tail = newNode;
+      this.size++;
     } else {
       newNode.nextNode = this.head;
       this.head = newNode;
+      this.size++;
     }
   }
 
   findNodeByKey(key) {
     if (this.head === null) {
-      console.log("The list is empty");
       return null;
     }
     let currentNode = this.head;
@@ -37,6 +39,41 @@ class LinkedList {
       }
     }
     return null;
+  }
+
+  removeNodeByKey(key) {
+    if (!this.head) {
+      return false;
+    }
+
+    if (this.head.key === key) {
+      this.head = this.head.nextNode;
+      this.size--;
+      if (this.head === null) {
+        this.tail = null;
+        this.size = 0;
+      }
+      return true;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.nextNode && currentNode.nextNode.key !== key) {
+      currentNode = currentNode.nextNode;
+    }
+
+    if (currentNode.nextNode && currentNode.nextNode.key === key) {
+      if (currentNode.nextNode === this.tail) {
+        this.tail = currentNode;
+        this.size--;
+        return true;
+      } else {
+        currentNode.nextNode = currentNode.nextNode.nextNode;
+        this.size--;
+        return true;
+      }
+    } else {
+      return false;
+    }
   }
 }
 
@@ -50,9 +87,17 @@ class HashMap {
   #hash(key) {
     let hashCode = 0;
 
+    let stringKey;
+    if (typeof key === "object") {
+      stringKey = JSON.stringify(key);
+    } else {
+      stringKey = key.toString();
+    }
+
     const primeNumber = 31;
-    for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.numBuckets;
+    for (let i = 0; i < stringKey.length; i++) {
+      hashCode =
+        (primeNumber * hashCode + stringKey.charCodeAt(i)) % this.numBuckets;
     }
 
     return hashCode;
@@ -62,7 +107,7 @@ class HashMap {
     let bucketIndex = this.#hash(key);
     let bucket = this.buckets[bucketIndex];
 
-    return bucket.findNodeByKey(key) ? true : false;
+    return bucket && bucket.findNodeByKey(key) ? true : false;
   }
 
   get(key) {
@@ -77,7 +122,28 @@ class HashMap {
     return node ? node.value : null;
   }
 
+  #resize() {
+    const oldBuckets = this.buckets;
+    this.numBuckets *= 2;
+    this.buckets = new Array(this.numBuckets).fill(null);
+
+    oldBuckets.forEach((bucket) => {
+      if (!bucket) {
+        return;
+      }
+      let currentNode = bucket.head;
+      while (currentNode) {
+        this.set(currentNode.key, currentNode.value);
+        currentNode = currentNode.nextNode;
+      }
+    });
+  }
+
   set(key, value) {
+    if (this.length() >= this.loadFactor * this.numBuckets) {
+      this.#resize();
+    }
+
     let bucketIndex = this.#hash(key);
     let bucket = this.buckets[bucketIndex];
 
@@ -100,17 +166,84 @@ class HashMap {
 
     if (bucket === null) {
       return false;
+    } else {
+      return bucket.removeNodeByKey(key);
     }
+  }
 
-    let node = this.get(key);
-    if (!node) {
-      return false;
+  length() {
+    let length = 0;
+    for (let bucket of this.buckets) {
+      if (bucket) {
+        length += bucket.size;
+      }
     }
+    return length;
+  }
 
-    //Je pense qu'il faut récupérer la fonction find(value)
-    //ça va nous donner l'index du node qu'on cherche
-    //puis puis utiliser removeAt()
-    //car on sait pas où est placé le node lié à key
-    //mais probablement pas besoin du "out of bounds"
+  clear() {
+    this.buckets = new Array(this.numBuckets).fill(null);
+  }
+
+  keys() {
+    let keysArray = [];
+    this.buckets.forEach((bucket) => {
+      if (!bucket) {
+        return;
+      }
+      let currentNode = bucket.head;
+      while (currentNode) {
+        keysArray.push(currentNode.key);
+        currentNode = currentNode.nextNode;
+      }
+    });
+    return keysArray;
+  }
+
+  values() {
+    let valuesArray = [];
+    this.buckets.forEach((bucket) => {
+      if (!bucket) {
+        return;
+      }
+      let currentNode = bucket.head;
+      while (currentNode) {
+        valuesArray.push(currentNode.value);
+        currentNode = currentNode.nextNode;
+      }
+    });
+    return valuesArray;
+  }
+
+  entries() {
+    let entriesArray = [];
+    this.buckets.forEach((bucket) => {
+      if (!bucket) {
+        return;
+      }
+      let currentNode = bucket.head;
+      while (currentNode) {
+        entriesArray.push([currentNode.key, currentNode.value]);
+        currentNode = currentNode.nextNode;
+      }
+    });
+    return entriesArray;
   }
 }
+
+const test = new HashMap();
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+test.set("moon", "silver");
+
+console.log(test.length());
